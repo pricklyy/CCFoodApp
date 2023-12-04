21f5349dc24aaa50a5accfd45d1922ac51f91b88
cuongto27535 <cuongtlph27535@fpt.edu.vn>
2023-12-04 17:31:35 +0700

    update fixadmin

--- app/src/main/java/com/example/cc_food/Admin/AdminActivity.java
+++ app/src/main/java/com/example/cc_food/Admin/AdminActivity.java
@@ -21,9 +21,15 @@ import com.example.cc_food.activities.ProductHiddenActivity;
 import com.example.cc_food.adapters.Billdetail_paid_Adapter;
 import com.example.cc_food.modules.billdetail_paid_model;
 
-cuongtlph27535
+
 import com.example.cc_food.ui.account.AccountManagerFragment;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
